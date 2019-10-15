49773da42242749bf7cd0d16f4cff486eb93ab7e
Frantisek Lachman <flachman@redhat.com>
2019-10-15 12:37:20 +0200

    Add function/method for logging imports
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- requre/__init__.py
+++ requre/__init__.py
@@ -1,3 +1,8 @@
-from requre.import_system import decorate, replace, replace_module
+from requre.import_system import decorate, replace, replace_module, log_imports
 
-__all__ = [decorate.__name__, replace.__name__, replace_module.__name__]
+__all__ = [
+    decorate.__name__,
+    replace.__name__,
+    replace_module.__name__,
+    log_imports.__name__,
+]
