530512fef31cd2c2455b9553992610bb50378f92
Jan Ščotka <jscotka@redhat.com>
2020-09-18 10:17:31 +0200

    precommit fix

--- tests/test_tempfile.py
+++ tests/test_tempfile.py
@@ -8,27 +8,32 @@ class TestTempFile(BaseClass):
     def testSimple(self):
         output = TempFile._mktemp()
         self.assertIn(
-            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_1", output,
+            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_1",
+            output,
         )
 
     def testChangeFile(self):
         self.cassette.storage_file += ".x"
         output = TempFile._mktemp()
         self.assertIn(
-            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_1", output,
+            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_1",
+            output,
         )
         output = TempFile._mktemp()
         self.assertIn(
-            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_2", output,
+            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_2",
+            output,
         )
         self.cassette.storage_file += ".y"
         self.assertEqual(TempFile.counter, 2)
         output = TempFile._mktemp()
         self.assertEqual(TempFile.counter, 1)
         self.assertIn(
-            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_1", output,
+            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_1",
+            output,
         )
         output = TempFile._mktemp()
         self.assertIn(
-            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_2", output,
+            f"/tmp/{os.path.basename(self.cassette.storage_file)}/static_tmp_2",
+            output,
         )
