b0d54fd6d5c34500a89060b243cb8d087a7d8004
Jan Ščotka <jscotka@redhat.com>
2020-10-30 11:14:49 +0100

    Allow to use list as parameter for decoratos and add common aliases for decorators.

---
+++ requre/modules_decorate_all_methods.py
@@ -0,0 +1,83 @@
+from typing import Optional, List
+from requre.online_replacing import (
+    replace_module_match,
+    apply_decorator_to_all_methods,
+    record_requests_for_all_methods,
+)
+from requre.helpers.tempfile import TempFile
+from requre.cassette import Cassette
+from requre.constants import TEST_METHOD_REGEXP
+from requre.helpers.git.repo import Repo
+from requre.helpers.git.fetchinfo import FetchInfoStorageList
+from requre.helpers.git.pushinfo import PushInfoStorageList
+from requre.helpers.files import StoreFiles
+
+module_requests = record_requests_for_all_methods
+
+
+def __generic(
+    _func=None,
+    cassette: Optional[Cassette] = None,
+    decorators: Optional[List] = None,
+    regexp_method_pattern=TEST_METHOD_REGEXP,
+):
+    if not decorators:
+        raise AttributeError("decorators parameter has to be defined")
+    decorator_list = []
+    for what, decorate in decorators:
+        decorator_list.append(
+            replace_module_match(what=what, decorate=decorate, cassette=cassette)
+        )
+    if _func is None:
+        return apply_decorator_to_all_methods(
+            decorator_list,
+            regexp_method_pattern=regexp_method_pattern,
+        )
+
+    return apply_decorator_to_all_methods(
+        decorator_list,
+        regexp_method_pattern=regexp_method_pattern,
+    )(_func)
+
+
+def module_tempfile(
+    _func=None,
+    cassette: Optional[Cassette] = None,
+    regexp_method_pattern=TEST_METHOD_REGEXP,
+):
+    decorators = [
+        ["tempfile.mkdtemp", TempFile.mkdtemp()],
+        ["tempfile.mktemp", TempFile.mktemp()],
+    ]
+    return __generic(
+        _func=_func,
+        cassette=cassette,
+        decorators=decorators,
+        regexp_method_pattern=regexp_method_pattern,
+    )
+
+
+def module_git(
+    _func=None,
+    cassette: Optional[Cassette] = None,
+    regexp_method_pattern=TEST_METHOD_REGEXP,
+):
+    decorators = [
+        [
+            "git.repo.base.Repo.clone_from",
+            StoreFiles.where_arg_references(
+                key_position_params_dict={"to_path": 2},
+                return_decorator=Repo.decorator_plain,
+                cassette=cassette,
+            ),
+        ],
+        ["git.remote.Remote.push", PushInfoStorageList.decorator_plain()],
+        ["git.remote.Remote.fetch", FetchInfoStorageList.decorator_plain()],
+        ["git.remote.Remote.pull", FetchInfoStorageList.decorator_plain()],
+    ]
+    return __generic(
+        _func=_func,
+        cassette=cassette,
+        decorators=decorators,
+        regexp_method_pattern=regexp_method_pattern,
+    )
