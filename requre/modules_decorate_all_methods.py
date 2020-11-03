c3f7b28132f21e3f67f8869b45e32206e9ac3490
Jan Ščotka <jscotka@redhat.com>
2020-11-03 15:38:13 +0100

    fix mistake

--- requre/modules_decorate_all_methods.py
+++ requre/modules_decorate_all_methods.py
@@ -17,7 +17,7 @@ record_requests_module = record_requests_for_all_methods
 
 
 def __replace_module_match_with_multiple_decorators(
-    *decorators: Callable,
+    *decorators: List,
     _func=None,
     cassette: Optional[Cassette] = None,
     regexp_method_pattern=TEST_METHOD_REGEXP,
