f07cdecf05aac9311c5897db5a1b14e679c6073e
Frantisek Lachman <flachman@redhat.com>
2023-10-30 17:10:02 +0100

    Provide __version__ of the package
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- requre/__init__.py
+++ requre/__init__.py
@@ -1,6 +1,9 @@
 # Copyright Contributors to the Packit project.
 # SPDX-License-Identifier: MIT
 
+import contextlib
+from importlib.metadata import PackageNotFoundError, distribution
+
 from requre.base_testclass import RequreTestCase
 from requre.record_and_replace import record, replace
 from requre.cassette import Cassette
@@ -8,6 +11,9 @@ from requre.simple_object import Simple, Tuple
 from requre.guess_object import Guess
 from requre.helpers.files import StoreFiles
 from requre.import_system import UpgradeImportSystem
+
+with contextlib.suppress(PackageNotFoundError):
+    __version__ = distribution(__name__).version
 
 __all__ = [
     record.__name__,
